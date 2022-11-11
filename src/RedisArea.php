c6828994d72db2302eee49f2d0dbc47c9e8c5744
杨友林 <xxyangyoulin@outlook.com>
2022-11-11 10:29:33 +0800

    🎸 feat: 省市县分开，返回值问题

--- src/RedisArea.php
+++ src/RedisArea.php
@@ -10,14 +10,15 @@ require_once root_path() . 'vendor/swg/composer/sdk/redis/Redis.php';
 /** redis中国地址库 */
 class RedisArea extends Redis
 {
-    /** @var string 省、市、区 键名 */
-    const REDIS_AREA_PROVINCE_KEY = 'area_province';
+    /** @var string 省份 */
     const REDIS_PROVINCE_KEY = 'address_province';
+    /** @var string 市前缀 下划线后拼接 省份id */
     const REDIS_CITY_KEY_PREF = 'address_city_';
+    /** @var string 县前缀 下划线后拼接 市id */
     const REDIS_COUNTY_KEY_PREF = 'address_county_';
+    /** @var string 省市县树形 */
     const REDIS_ALL_PARENT_KEY = 'address_all_parent';
-
-    /** @var string 街道键 下划线后拼接 区级编号 */
+    /** @var string 街道键 下划线后拼接 县id */
     const REDIS_STREET_KEY_PREF = 'area_street_';
 
     public function __construct()
@@ -26,56 +27,120 @@ class RedisArea extends Redis
         $this->redis->select(self::REDIS_AREA_DB);
     }
 
+
+    #=======【 省市县树形 】=====================================
+
     /**
-     * 添加所有的省市县数据
+     * 添加省市县树形
+     * @param array $data 树形数组
      */
-    public function addAllParentAddress($data)
+    public function addAllParentAddress(array $data)
     {
         $this->setAddressData(self::REDIS_ALL_PARENT_KEY, $data);
     }
 
-    public function getAllParentAddress()
+    /**
+     * 获取省市县树结构
+     * @return array|null 省市县树形数组
+     */
+    public function getAllParentAddress(): ?array
     {
         return $this->getAddressData(self::REDIS_ALL_PARENT_KEY);
     }
 
+    #=======【 省 】=====================================
+
+    /**
+     * 添加省份信息
+     */
+    public function addProvince(array $data)
+    {
+        $this->setAddressData(self::REDIS_PROVINCE_KEY, $data);
+    }
+
+    /**
+     * 获取所有省份信息
+     * @return array
+     */
+    public function getProvince(): ?array
+    {
+        return $this->getAddressData(self::REDIS_PROVINCE_KEY);
+    }
+
+    #=======【 市 】=====================================
+
+    /**
+     * 添加某个省份的城市信息
+     * @param int $province_id 省份id
+     * @param $data
+     */
+    public function addCityOfProvince(int $province_id, $data)
+    {
+        $this->setAddressData(self::REDIS_CITY_KEY_PREF . $province_id, $data);
+    }
+
+    /**
+     * 获取某个省份的城市信息
+     * @param int $province_id 省份id
+     * @return array|null 城市列表
+     */
+    public function getCityOfProvince(int $province_id): ?array
+    {
+        return $this->getAddressData(self::REDIS_CITY_KEY_PREF . $province_id);
+    }
+
+    #=======【 县 】=====================================
+
+    /**
+     * 添加某个市的县信息
+     * @param int $city_id 市id
+     * @param $data
+     */
+    public function addCountyOfCity(int $city_id, $data)
+    {
+        $this->setAddressData(self::REDIS_COUNTY_KEY_PREF . $city_id, $data);
+    }
+
+    /**
+     * 获取某个市的县信息
+     * @param int $city_id 市id
+     * @return array|null 县区列表
+     */
+    public function getCountyOfCity(int $city_id): ?array
+    {
+        return $this->getAddressData(self::REDIS_COUNTY_KEY_PREF . $city_id);
+    }
+
+
+    #=======【 街 】=====================================
+
     /**
      * 添加某个县的街道
+     * @param int $county_id 县id
+     * @param array $data
      */
-    public function addTownOfCounty($county_id, $data)
+    public function addTownOfCounty(int $county_id, array $data)
     {
         $this->setAddressData(self::REDIS_STREET_KEY_PREF . $county_id, $data);
     }
 
     /**
      * 获取某个县的街道
+     * @param int $county_id 县区id
+     * @return array|null 街道列表
      */
-    public function getTownOfCounty($county_id)
+    public function getTownOfCounty(int $county_id): ?array
     {
-        $this->getAddressData(self::REDIS_STREET_KEY_PREF . $county_id);
+        return $this->getAddressData(self::REDIS_STREET_KEY_PREF . $county_id);
     }
 
-    /**
-     * 设置区域信息【省、市、区 存一个字段】
-     * 数据格式需和前端沟通，前端怎么展示方便怎么存储
-     * Author: zhouhongcheng
-     * datetime 2022/11/4 18:29
-     * @param string $area_key 键
-     * @param array $data 地址数据
-     * @return bool
-     */
-    public function setAddressData(string $area_key, array $data)
+    #=======【 set/get 】=====================================
+
+    public function setAddressData(string $area_key, array $data): bool
     {
         return $this->redis->set($area_key, json_encode($data));
     }
 
-    /**
-     * 获取区域信息
-     * Author: zhouhongcheng
-     * datetime 2022/11/4 18:30
-     * @param string $area_key 键
-     * @return mixed
-     */
     public function getAddressData(string $area_key)
     {
         return json_decode($this->redis->get($area_key));
