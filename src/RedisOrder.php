fcb004542c8c32334c76733d08f7e329e538a88c
吕刚 <15985691662@163.com>
2022-11-17 15:34:00 +0800

    订单退款，修改地址存入Redis

--- src/RedisOrder.php
+++ src/RedisOrder.php
@@ -2,6 +2,7 @@
 
 namespace Swg\Composer;
 
+use RedisException;
 use Swg\Redis\Redis;
 
 require_once 'sdk/redis/Redis.php';
@@ -15,6 +16,15 @@ class RedisOrder extends Redis
 
     /** @var string C端订单 */
     const REDIS_CUSTOMER_ORDER_KEY = 'customer_order';
+
+    /** @var string C端订单退款中 */
+    const REDIS_CUSTOMER_ORDER_REFUND_KEY = 'customer_order_refund';
+    /** @var string C端订单退款被驳回 */
+    const REDIS_CUSTOMER_ORDER_REFUND_REJECT_KEY = 'customer_order_reject';
+    /** @var string C端订单退款成功 */
+    const REDIS_CUSTOMER_ORDER_REFUND_SUCCESS_KEY = 'customer_order_success';
+    /** @var string 订单修改地址 */
+    const REDIS_CUSTOMER_ORDER_UPDATE_ADDRESS_KEY = 'customer_order_update_address';
 
 
     public function __construct()
@@ -75,5 +85,57 @@ class RedisOrder extends Redis
     public function getCustomerOrder()
     {
         return $this->redis->rPop(self::REDIS_CUSTOMER_ORDER_KEY);
+    }
+
+    /**
+     * C端订单退款申请成功写入订单
+     * Author: lvg
+     * datetime 2022/11/17 10:25
+     * @param string $order_sn 订单编号
+     * @return false|int|\Redis
+     * @throws RedisException
+     */
+    public function pushCustomerOrderRefund(string $order_sn)
+    {
+        return $this->redis->lpush(self::REDIS_CUSTOMER_ORDER_REFUND_KEY, $order_sn);
+    }
+
+    /**
+     * C端订单退款申请驳回写入订单
+     * Author: lvg
+     * datetime 2022/11/17 10:25
+     * @param string $order_sn 订单编号
+     * @return false|int|\Redis
+     * @throws RedisException
+     */
+    public function pushCustomerOrderRefundReject(string $order_sn)
+    {
+        return $this->redis->lpush(self::REDIS_CUSTOMER_ORDER_REFUND_REJECT_KEY, $order_sn);
+    }
+
+    /**
+     * C端订单退款成功写入订单
+     * Author: lvg
+     * datetime 2022/11/17 10:25
+     * @param string $order_sn 订单编号
+     * @return false|int|\Redis
+     * @throws RedisException
+     */
+    public function pushCustomerOrderRefundSuccess(string $order_sn)
+    {
+        return $this->redis->lpush(self::REDIS_CUSTOMER_ORDER_REFUND_SUCCESS_KEY, $order_sn);
+    }
+
+    /**
+     * C端修改地址成功写入订单
+     * Author: lvg
+     * datetime 2022/11/17 10:25
+     * @param string $order_sn 订单编号
+     * @return false|int|\Redis
+     * @throws RedisException
+     */
+    public function pushCustomerOrderUpdateAddress(string $order_sn)
+    {
+        return $this->redis->lpush(self::REDIS_CUSTOMER_ORDER_UPDATE_ADDRESS_KEY, $order_sn);
     }
 }
