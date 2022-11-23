fbfa1c0da9d0488e5403d603a0c569227c024f3e
周洪成 <zhouhc@jqlink.cn>
2022-11-23 20:19:48 +0800

    bug

--- src/Encrypt.php
+++ src/Encrypt.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Swg\Composer;
 
 /** 加密、解密、密码生成 */
@@ -14,9 +15,9 @@ class Encrypt
      * @param string $secret_key 加密秘钥
      * @return string
      */
-    public function encrypt(string $encrypt_str,string $secret_key = ''): string
+    public function encrypt(string $encrypt_str, string $secret_key = ''): string
     {
-        return openssl_encrypt($encrypt_str, 'des-ecb', $secret_key?:env("PASSWORD.SECRET_KEY"));
+        return openssl_encrypt($encrypt_str, 'des-ecb', $secret_key ?: env("PASSWORD.SECRET_KEY"));
     }
 
     /**
@@ -29,9 +30,9 @@ class Encrypt
      * @param string $secret_key 解密秘钥
      * @return string
      */
-    public function decrypt(string $decrypt_str,string $secret_key = ''): string
+    public function decrypt(string $decrypt_str, string $secret_key = ''): string
     {
-        return openssl_decrypt($decrypt_str, 'des-ecb',$secret_key?:env("PASSWORD.SECRET_KEY"));
+        return openssl_decrypt($decrypt_str, 'des-ecb', $secret_key ?: env("PASSWORD.SECRET_KEY"));
     }
 
     /**
@@ -44,9 +45,9 @@ class Encrypt
      * @param string $salt 盐
      * @return string
      */
-    public function createPassword(string $password,string $salt = '') : string
+    public function createPassword($password, string $salt = ''): string
     {
-        return md5($salt.$password.$salt);
+        return md5($salt . $password . $salt);
     }
 
     /**
@@ -62,13 +63,12 @@ class Encrypt
     {
         $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_ []{}<>~`+=,.;:/?|';
         $salt = '';
-        for ( $i = 0; $i < $length; $i++ )
-        {
+        for ($i = 0; $i < $length; $i++) {
             // 这里提供两种字符获取方式
             // 第一种是使用 substr 截取$chars中的任意一位字符；
             // 第二种是取字符数组 $chars 的任意元素
             // $password .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
-            $salt .= $chars[ mt_rand(0, strlen($chars) - 1) ];
+            $salt .= $chars[mt_rand(0, strlen($chars) - 1)];
         }
         return $salt;
     }
