5b58984b7e18357fd7573c0dfced3a591df6bf67
吕刚 <15985691662@163.com>
2022-11-30 13:22:27 +0800

    配置写入Redis

--- src/RedisConfig.php
+++ src/RedisConfig.php
@@ -1,24 +1,58 @@
 <?php
+
 namespace Swg\Composer;
 
 use Swg\Redis\Redis;
+
 // require_once 'sdk/redis/Redis.php';
-require_once root_path() .'vendor/swg/composer/sdk/redis/Redis.php';
+require_once root_path() . 'vendor/swg/composer/sdk/redis/Redis.php';
 
 /** redis配置库 */
 class RedisConfig extends Redis
 {
-    /** @var string B端配置 redis前缀 */
-    CONST REDIS_BUSINESS_CONFIG_KEY = 'business_config';
-
-    /** @var string 服务端配置 redis前缀 */
-    CONST REDIS_SERVICE_CONFIG_KEY = 'service_config';
-
-
     public function __construct()
     {
         parent::__construct();
         $this->redis->select(self::REDIS_CONFIG_DB);
     }
 
+    /**
+     * 设置配置
+     * Author: lvg
+     * datetime 2022/11/30 13:20
+     * @param string $module
+     * @param string $name
+     * @param array $data
+     * @return bool
+     */
+    public function setConfig(string $module, string $name, array $data): bool
+    {
+        return $this->setData($module . '_' . $name, $data);
+    }
+
+    /**
+     * 删除配置
+     * Author: lvg
+     * datetime 2022/11/30 13:20
+     * @param string $module
+     * @param string $name
+     * @return int
+     */
+    public function delConfig(string $module, string $name): int
+    {
+        return $this->delete($module . '_' . $name);
+    }
+
+    /**
+     * 获取配置
+     * Author: lvg
+     * datetime 2022/11/30 13:21
+     * @param string $module
+     * @param string $name
+     * @return array|mixed|null
+     */
+    public function getConfig(string $module, string $name)
+    {
+        return $this->getData($module . '_' . $name);
+    }
 }
